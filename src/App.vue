9b0ecabbba48f7f7daf9a63f96d879a4c2a46c0d
Gitea <gitea@fake.local>
2018-03-30 14:42:55 +0800

    npm install firebase vuefire --save
    ```bash
    cd please-introduce-yourself
    npm install firebase vuefire --save
    ```
    ```bash
    cd please-introduce-yourself
    npm run dev
    ```
    
    grpc依赖protobuf，所以需要安装protobuf，下载地址:https://github.com/google/protobuf/releases
    
    解压protoc-3.0.0-win32.zip，然后把解压后的文件protoc-3.0.0-win32下bin子目录添加到系统的环境变量，比如我的目录地址是：D:\Program Files\protoc-3.0.0-win32\bin，查看是否成功，同时按win+R，在对话框输入cmd，然后在命令行里面输入protoc --version，出现版本信息则表示成功：

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div v-for="message in messages"
+      :key="message.id">
+      <h4>{{ message.title }}</h4>
+      <p>{{ message.text }}</p>
+      <p>{{ message.timestamp }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase'
+
+let config = {
+  apiKey: 'AIzaSyAM_Q7pMQnbCXbvxJE19s4Gw312hZuIEoM',
+  authDomain: 'presentfloyd.firebaseapp.com',
+  databaseURL: 'https://presentfloyd.firebaseio.com',
+  projectId: 'presentfloyd',
+  storageBucket: 'presentfloyd.appspot.com',
+  messagingSenderId: '801323269585'
+}
+
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let messagesRef = db.ref('messages')
 export default {
-  name: 'App'
+  firebase: {
+    messages: messagesRef
+  }
 }
 </script>
 
