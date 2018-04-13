807951e5ad4317ce251487f7abde0b326d94e58f
790013438 <790013438@users.noreply.github.com>
2018-04-13 17:59:56 +0800

    概括图

--- profitoro/src/App.vue
+++ profitoro/src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h1>Welcome to Profitoro</h1>
+    <landing-page></landing-page>
   </div>
 </template>
 
 <script>
+import LandingPage from './components/LandingPage'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    LandingPage
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
