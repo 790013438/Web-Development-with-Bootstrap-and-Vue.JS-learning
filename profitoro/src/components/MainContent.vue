29c51d589308eb6826694da8d8f528779eff1e81
790013438 <790013438@qq.com>
2018-04-14 13:34:53 +0800

    components

--- profitoro/src/components/MainContent.vue
+++ profitoro/src/components/MainContent.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>Main Content</div>
+  <div>
+    <header-component></header-component>
+    <content-component></content-component>
+    <footer-component></footer-component>
+  </div>
 </template>
 
 <script>
+import {ContentComponent, FooterComponent, HeaderComponent} from './main'
 export default {
-
+  components: {
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent
+  }
 }
 </script>
 
--- profitoro/src/components/main/ContentComponent.vue
+++ profitoro/src/components/main/ContentComponent.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>Content</div>
+  <div>
+    <pomodoro-timer></pomodoro-timer>
+  </div>
 </template>
 
 <script>
+import {PomodoroTimer} from './sections'
 export default {
-
+  components: {
+    PomodoroTimer
+  }
 }
 </script>
 
