01d999d3328e50d93fe5f16c4cc33e820d711d4e
790013438 <790013438@users.noreply.github.com>
2018-04-16 16:44:34 +0800

    解决console报错问题，通过新的svg属性
    ```vue
    <div class="circle">
      <svg class="timer" viewBox="0 0 200 200" preserveAspectRatio="xMinYMin meet" xmlns="http://www.w3.org/2000/svg">
        <circle class="bigCircle" r="100" cx="100" cy="100"></circle>
        <circle class="smallCircle" r="90" cx="100" cy="100"></circle>
        <path class="segment" id="sector" opacity="0.6"
          :d="path"></path>
        <text class="text" v-if="text != ''" x="100" y="100">{{text}}</text>
      </svg>
    </div>
    ```

--- profitoro/src/components/main/sections/timer/SVGCircleSector.vue
+++ profitoro/src/components/main/sections/timer/SVGCircleSector.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
-    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-      <circle r="100" cx="100" cy="100" fill="gray"></circle>
-      <circle r="90" cx="100" cy="100" fill="lightgray"></circle>
-      <path id="sector" fill="darkgray" opacity="0.6"
+  <div class="circle">
+    <svg class="timer" viewBox="0 0 200 200" preserveAspectRatio="xMinYMin meet" xmlns="http://www.w3.org/2000/svg">
+      <circle class="bigCircle" r="100" cx="100" cy="100"></circle>
+      <circle class="smallCircle" r="90" cx="100" cy="100"></circle>
+      <path class="segment" id="sector" opacity="0.6"
         :d="path"></path>
+      <text class="text" v-if="text != ''" x="100" y="100">{{text}}</text>
     </svg>
   </div>
 </template>
 
 <script>
+/**
+ * Calculates the x,y coordinates on the circumference for the given angle
+ * @param {number} angle
+ * @returns {object}
+ */
 function calcEndPoint (angle) {
   let x, y
   x = 100 - 100 * Math.sin(Math.PI * angle / 180)
@@ -19,6 +25,11 @@ function calcEndPoint (angle) {
     x, y
   }
 }
+/**
+ * Calculates the path attribute for the svg element to draw a circle sector for the given angle
+ * @param {number} angle
+ * @returns {string}
+ */
 function calcPath (angle) {
   let d
   let {x, y} = calcEndPoint(angle)
@@ -41,4 +52,38 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$big-circle-color: gray;
+$small-circle-color: lightgray;
+$segment-color: darkgray;
+$text-color: black;
+
+.circle {
+  display: inlin-block;
+  position: relative;
+  width: 100%;
+  padding-bottom: 100%;
+  vertical-align: middle;
+  svg {
+    display: inline-block;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+
+.bigCircle {
+  fill: $big-circle-color;
+}
+.smallCircle {
+  fill: $small-circle-color;
+}
+.segment {
+  fill: $segment-color;opacity: 0.6;
+}
+.text {
+  font-size: 1em;
+  stroke-width: 0;
+  opacity: .9;
+  fill: $text-color;
+}
 </style>
