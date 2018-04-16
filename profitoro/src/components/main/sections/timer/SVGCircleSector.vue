a079fba9b20340303774fb4388f82d799ac3105f
790013438 <790013438@users.noreply.github.com>
2018-04-16 10:45:05 +0800

    v-bind,computed: {path () {return ..}}计算图形

--- profitoro/src/components/main/sections/timer/SVGCircleSector.vue
+++ profitoro/src/components/main/sections/timer/SVGCircleSector.vue
@@ -3,14 +3,26 @@
     <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
       <circle r="100" cx="100" cy="100" fill="gray"></circle>
       <circle r="90" cx="100" cy="100" fill="lightgray"></circle>
-      <path id="sector" fill="darkgray" opacity="0.6" d="M100,100 V0 L100,0 A100,100 0 0, 0 0, 100 z"></path>
+      <path id="sector" fill="darkgray" opacity="0.6"
+        :d="path"></path>
     </svg>
   </div>
 </template>
 
 <script>
+function calcPath (angle) {
+  let d
+  d = 'M100,100 L100,0 A100,100 0 0, 0 0, 100 z'
+  return d
+}
+
 export default {
-  props: ['angle', 'text']
+  props: ['angle', 'text'],
+  computed: {
+    path () {
+      return calcPath(this.angle)
+    }
+  }
 }
 </script>
 
