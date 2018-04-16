6e6c381d773d8b39ba7c5b18831c80d1e3489609
790013438 <790013438@users.noreply.github.com>
2018-04-16 10:21:46 +0800

    svg扇形

--- profitoro/src/components/main/sections/timer/SVGCircleSector.vue
+++ profitoro/src/components/main/sections/timer/SVGCircleSector.vue
@@ -3,6 +3,7 @@
     <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
       <circle r="100" cx="100" cy="100" fill="gray"></circle>
       <circle r="90" cx="100" cy="100" fill="lightgray"></circle>
+      <path id="sector" fill="darkgray" opacity="0.6" d="M100,100 V0 L100,0 A100,100 0 0, 0 0, 100 z"></path>
     </svg>
   </div>
 </template>
