724b1c89e4e6878f66be7a1271a75621c40e0aaf
790013438 <790013438@users.noreply.github.com>
2018-04-19 17:14:38 +0800

    设置Setting

--- profitoro/src/components/main/sections/Settings.vue
+++ profitoro/src/components/main/sections/Settings.vue
@@ -8,18 +8,46 @@
     </div>
     <div class="col-sm-12 col-md-6 col-lg-8">
       <div class="container">
-      <h2>Set your pomodoro timer</h2>
-      pomodoro timer configuration
+        <h2>Set your pomodoro timer</h2>
+        <div class="row justify-content-center align-items-center">
+          <div class="col-md-5 col-sm-10">
+            <set-timer
+               :value="config.workingPomodoro"></set-timer>
+             <div class="figure-caption">Pomodoro</div>
+          </div>
+          <div class="col-md-4 col-sm-10">
+            <set-timer
+               :value="config.longBreak"></set-timer>
+             <div class="figure-caption">Long break</div>
+          </div>
+          <div class="col-md-3 col-sm-10">
+            <set-timer
+               :value="config.shortBreak"></set-timer>
+             <div class="figure-caption">Short break</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
+import SetTimer from './timer/SetTimer'
 export default {
-
+  computed: {
+    ...mapGetters({
+      config: 'getConfig'
+    })
+  },
+  components: {
+    SetTimer
+  }
 }
 </script>
 
-<style scoped lang="sass">
+<style scoped lang="scss">
+.figure-caption {
+  text-align: center;
+}
 </style>
