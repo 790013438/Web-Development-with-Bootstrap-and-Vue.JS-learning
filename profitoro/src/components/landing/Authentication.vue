a795dc1caac5a43247752b00f70912fe55151bf1
790013438 <790013438@qq.com>
2018-04-22 16:05:43 +0800

    form表单, vue事件
    先html
    再js，vue
    然后绑定事件，@
    ```vue
    <button
      @click="onRegisterClick">Register!</button>
    <button
      @click="onLoginClick">Log in!</button>
    ```

--- profitoro/src/components/landing/Authentication.vue
+++ profitoro/src/components/landing/Authentication.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>Authentication</div>
+  <div>
+    <h1>Register</h1>
+    <input v-model="registerEmail" type="text" placeholder="email"/>
+    <input v-model="registerPassword" type="password" placeholder="password"/>
+    <button
+      @click="onRegisterClick">Register!</button>
+    <h1>Login</h1>
+    <input v-model="loginEmail" type="textt" placeholder="email"/>
+    <input v-model="loginPassword" type="password" placeholder="password"/>
+    <button
+      @click="onLoginClick">Log in!</button>
+  </div>
 </template>
 
 <script>
-export default {
+import {mapActions} from 'vuex'
 
+export default {
+  data () {
+    return {
+      registerEmail: '',
+      registerPassword: '',
+      loginEmail: '',
+      loginPassword: ''
+    }
+  },
+  methods: {
+    ...mapActions(['createUser', 'authenticate']),
+    onRegisterClick () {
+      this.createUser({email: this.registerEmail, password: this.registerPassword})
+    },
+    onLoginClick () {
+      this.authenticate({email: this.loginEmail, password: this.loginPassword})
+    }
+  }
 }
 </script>
 
