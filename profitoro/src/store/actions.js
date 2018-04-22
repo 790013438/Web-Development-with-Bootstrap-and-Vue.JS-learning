69515f29e0335250ee6d9022ef79f772dabe2962
790013438 <790013438@qq.com>
2018-04-22 08:59:04 +0800

    更新database, update firebase,修改数据

--- profitoro/src/store/actions.js
+++ profitoro/src/store/actions.js
@@ -1,18 +1,36 @@
 import { firebaseAction } from 'vuexfire'
 
 export default {
-  setWorkingPomodoro ({commit}, workingPomodoro) {
-    if (workingPomodoro) {
+  setWorkingPomodoro ({commit, state}, workingPomodoro) {
+    if (!workingPomodoro) {
+      return
+    }
+    if (state.configRef) {
+      workingPomodoro = parseInt(workingPomodoro, 10)
+      state.configRef.update({workingPomodoro})
+    } else {
       commit('setWorkingPomodoro', parseInt(workingPomodoro, 10))
     }
   },
-  setShortBreak ({commit}, shortBreak) {
-    if (shortBreak) {
+  setShortBreak ({commit, state}, shortBreak) {
+    if (!shortBreak) {
+      return
+    }
+    if (state.configRef) {
+      shortBreak = parseInt(shortBreak, 10)
+      state.configRef.update({shortBreak})
+    } else {
       commit('setShortBreak', parseInt(shortBreak, 10))
     }
   },
-  setLongBreak ({commit}, longBreak) {
-    if (longBreak) {
+  setLongBreak ({commit, state}, longBreak) {
+    if (!longBreak) {
+      return
+    }
+    if (state.configRef) {
+      longBreak = parseInt(longBreak, 10)
+      state.configRef.update({longBreak})
+    } else {
       commit('setLongBreak', parseInt(longBreak, 10))
     }
   },
